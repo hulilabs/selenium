d9e1045b4cd6de7b976b1f3ce4180c7f4b5c137e
Victor <victorsm0902@gmail.com>
2016-01-26 17:15:33 -0600

    Still testing

--- selenium/src/test/java/Testng2.java
+++ selenium/src/test/java/Testng2.java
@@ -34,8 +34,7 @@ public class Testng2 {
 
   @Test(description = "Open create account page")
   public void title() throws InterruptedException{
-    driver.get("http://huli-phr#login");
-    driver.get("http://huli-phr#login");
+    /*driver.get("http://huli-phr#login");
     System.out.println("Viene el body");
     System.out.println(driver.findElement( By.tagName("body")).getText());
     Thread.sleep(4000);
@@ -44,11 +43,11 @@ public class Testng2 {
     Thread.sleep(4000);
     WebElement boton2 = driver.findElement(By.id("back-action"));
     System.out.println(boton2.getText());
-    Assert.assertEquals("Ya tengo una cuenta", boton2.getText(),"The account creation page wasn't opened correctly");
-    /*driver.get("http://www.google.com");
+    Assert.assertEquals("Ya tengo una cuenta", boton2.getText(),"The account creation page wasn't opened correctly");*/
+    driver.get("http://www.google.com");
     String titles = driver.getTitle();
     Assert.assertEquals(titles, "Google"); 
-    System.out.println("Numero 1");*/
+    System.out.println("Numero 1");
   }
  
 }
