1bd6ff59dd908776fac298f18cbe658388355618
Victor <victorsm0902@gmail.com>
2016-01-26 17:32:57 -0600

    Added time to print body content to check the error in firefox

--- selenium/src/test/java/Testng2.java
+++ selenium/src/test/java/Testng2.java
@@ -34,20 +34,20 @@ public class Testng2 {
 
   @Test(description = "Open create account page")
   public void title() throws InterruptedException{
-    /*driver.get("http://huli-phr#login");
+    driver.get("http://huli-phr#login");
+    Thread.sleep(4000);
     System.out.println("Viene el body");
     System.out.println(driver.findElement( By.tagName("body")).getText());
-    Thread.sleep(4000);
     WebElement boton = driver.findElement(By.id("register-action"));
     boton.click();
     Thread.sleep(4000);
     WebElement boton2 = driver.findElement(By.id("back-action"));
     System.out.println(boton2.getText());
-    Assert.assertEquals("Ya tengo una cuenta", boton2.getText(),"The account creation page wasn't opened correctly");*/
-    driver.get("http://www.google.com");
+    Assert.assertEquals("Ya tengo una cuenta", boton2.getText(),"The account creation page wasn't opened correctly");
+    /*driver.get("http://www.google.com");
     String titles = driver.getTitle();
     Assert.assertEquals(titles, "Google"); 
-    System.out.println("Numero 1");
+    System.out.println("Numero 1");*/
   }
  
 }
